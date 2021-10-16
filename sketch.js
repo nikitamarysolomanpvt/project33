855db7b9d3bd582d7c4210176f20b2ab671fa0f3
nikitamarysolomanpvt <54615666+nikitamarysolomanpvt@users.noreply.github.com>
2021-10-16 20:35:19 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,11 +7,11 @@ var engine, world;
 var backgroundImg;
 var hour;
 
-var bg = "sunrise.png";
+var bg = "sunrise1.png";
 
 function preload() {
+    // create getBackgroundImg( ) here
     getBackgroundImg();
-    
 }
 
 function setup(){
@@ -22,34 +22,37 @@ function setup(){
 }
 
 function draw(){
+     // add condition to check if any background image is there to add
     if(backgroundImg)
-        background(backgroundImg);
+    background(backgroundImg);
 
     Engine.update(engine);
+    // write code to display time in correct format here
     fill("black");
     textSize(30);
+
     if(hour>=12){
         text("Time : "+ hour%12 + " PM", 50,100);
-    }else if(hour==0){
-        text("Time : 12 AM",100,100);
-    }else{
+       }else if(hour==0){
+         text("Time : 12 AM",100,100);
+       }else{
         text("Time : "+ hour%12 + " AM", 50,100);
-    }      
-    
+       }
 
 }
 
 async function getBackgroundImg(){
-var response = await fetch("https://worldtimeapi.org/api/timezone/Asia/kolkata");
-    var responseJSON= await response.json();
-var datetime = responseJSON.datetime
-console.log(datetime);
-var hour = datetime.slice(11,13);
-console.log(hour);
 
+    // write code to fetch time from API
+    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
 
-
+    //change the data in JSON format
+    var responseJSON = await response.json();
+    var datetime = responseJSON.datetime;
     
+    // write code slice the datetime
+    hour = datetime.slice(11,13);
+
     if(hour>=0 && hour<18 ){
         bg = "sunrise.png";
     }
